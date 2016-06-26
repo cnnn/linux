5923b3aa8f7f0bed13e650d3164e11067a228e7b
cnnn <cnnn@me.com>
2016-06-26 10:31:28 +0300

    Test: add OPPO 105D USB for DSD-native

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -1203,6 +1203,7 @@ u64 snd_usb_interface_dsd_format_quirks(struct snd_usb_audio *chip,
 	case USB_ID(0x20b1, 0x2008): /* Matrix Audio X-Sabre */
 	case USB_ID(0x20b1, 0x300a): /* Matrix Audio Mini-i Pro */
 	case USB_ID(0x22d9, 0x0416): /* OPPO HA-1 */
+	case USB_ID(0x22d9, 0x0451): /* OPPO 105D */
 		if (fp->altsetting == 2)
 			return SNDRV_PCM_FMTBIT_DSD_U32_BE;
 		break;
